683ffd37d88cc2671ea5ba855fae0e29c280a0de
elainedevAccount <20067964@mail.wit.ie>
2024-10-27 12:22:34 +0000

    Added Upcoming movies page

---
+++ movies/src/pages/upcomingMoviesPage.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { getUpcoming } from "../api/tmdb-api";
+import PageTemplate from '../components/templatesMovieListPage';
+import { useQuery } from 'react-query';
+import Spinner from '../components/spinner';
+import AddToFavouritesIcon from '../components/cardIcons/addToFavourites'
+
+const UpcomingMovies = (props) => {
+
+  const {  data, error, isLoading, isError }  = useQuery('upcoming', getUpcoming)
+
+  if (isLoading) {
+    return <Spinner />
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>
+  }  
+  const movies = data.results;
+
+  // Redundant, but necessary to avoid app crashing.
+  const favourites = movies.filter(m => m.favorite)
+  localStorage.setItem('favourites', JSON.stringify(favourites))
+  const addToFavourites = (movieId) => true 
+
+  return (
+    <PageTemplate
+      title="Upcoming Movies"
+      movies={movies}
+      action={(movie) => {
+        return <AddToFavouritesIcon movie={movie} />
+      }}
+    />
+);
+};
+export default UpcomingMovies;
