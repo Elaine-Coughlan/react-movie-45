37e5d7e7629f9b3c7f0f5ce00af71408ee846ae0
elainedevAccount <20067964@mail.wit.ie>
2024-11-07 16:51:07 +0000

    TVShows card with sample data

--- movies/src/pages/tvShowsHomePage.js
+++ movies/src/pages/tvShowsHomePage.js
@@ -1,15 +1,44 @@
 import React from "react";
 import Grid from "@mui/material/Grid2";
+import ShowsCard from "../components/tvshows/cardcomponent";
 
 const TVShowsHomePage = (props) => {
-  const shows = props.shows;
+  const sample = {
+
+    "adult": false,
+    "backdrop_path": "/aizbHLcKVWvJ7jxkflJzTu5Z8GE.jpg",
+    "genre_ids": [
+      10766
+    ],
+    "id": 81329,
+    "origin_country": [
+      "FR"
+    ],
+    "original_language": "fr",
+    "original_name": "Un si grand soleil",
+    "overview": "Claire is surprised when she gets arrested for the murder of her childhood friend after she returns to Montpellier.",
+    "popularity": 3519.173,
+    "poster_path": "/t6jVlbPMtZOJoAOfeoR4yQmnjXM.jpg",
+    "first_air_date": "2018-08-27",
+    "name": "Chronicles of the Sun",
+    "vote_average": 7,
+    "vote_count": 107
+  };
+
+     
+
+const shows = [sample, sample, sample];
     
   return (
-  <Grid container>
-      <Grid size={12}>
-          <h1> HomePage </h1>
+    <Grid container>
+        <Grid size={12}>
+            <h1> HomePage </h1>
+        </Grid>
+        <Grid size={3}>
+          <ShowsCard show={shows[0]} />
+        </Grid>
       </Grid>
-    </Grid>
-  );
-};
+    );
+  };
 export default TVShowsHomePage;
+
